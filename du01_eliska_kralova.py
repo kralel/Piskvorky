9ac382a319c47e402c798a32dbac6652e143b469
Eliška Králová <kralovaeliska3@natur.cuni.cz>
2022-10-29 20:03:43 +0200

    verze2

--- du01_eliska_kralova.py
+++ du01_eliska_kralova.py
@@ -2,28 +2,60 @@
 # hráč vyhrál, ani kolizi v políčkách. Hráči zadávají souřadnice do konzole,
 # průběh hry je vykreslen želví grafikou
 
-from turtle import forward, exitonclick, left, right, circle, speed
+from turtle import forward, exitonclick, left, right, circle, speed, setpos, hideturtle, pendown, penup, home
+from math import sqrt
 
 a = int(input("Zadej počet sloupců:"))
 b = int(input("Zadej počet řádků:"))
 strana = 50
+uhlopricka = sqrt(strana**2+strana**2)
+def krizek(x, y):
+	penup()
+	setpos(x*strana, y*strana)
+	right(45)
+	penup()
+	forward(10)
+	pendown()
+	forward(uhlopricka-20)
+	penup()
+	forward(10)
+	right(135)
+	forward(strana)
+	right(135)
+	forward(10)
+	pendown()
+	forward(uhlopricka-20)
+	penup()
+	home()
+	
 
 speed(10)
+#hideturtle()
 
+rows=[]
 for y in range(b):
+	rows.append([])
 	for x in range(a):
+		rows[y].append(0)
 		for i in range(4):
 			forward(strana)
-			left(90)
+			right(90)
 		forward(strana)
-	right(180)
+	left(180)
 	forward(a*strana)
-	right(90)
+	left(90)
 	forward(strana)
-	right(90)
+	left(90)
 
 speed("normal")
 
-circle(15)
+while rows != [1, 1, 1]:
+	x = int(input("Zadej číslo sloupce:"))
+	y = int(input("Zadej číslo řádku:"))
+	krizek(x-1, -(y-1))	# Turtle - poč. souřadnic vlevo dole X hráč - poč. souřadnic vlevo nahoře => -y
+
+if rows == [1, 1, 1]:
+	print("Hrací pole je plné! Konec hry.")
+	exit()
 
 exitonclick()
