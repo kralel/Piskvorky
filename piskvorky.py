810166544ccc63eab9a4b81c8d17d451dc335476
Eliška Králová <kralovaeliska3@natur.cuni.cz>
2023-02-05 10:37:32 +0100

    Funkční minimax

--- piskvorky.py
+++ piskvorky.py
@@ -1,6 +1,5 @@
 from tkinter import *
-import math, copy
-from collections import namedtuple
+import math
 
 strana = 25
 
@@ -9,61 +8,53 @@ class Piskvorky:
 	sirka = 0
 	vyska = 0
 	plan=[]
-	tahy = 0										# Počet umístěných znaků
+	#: jsou tahy potřeba?
+	tahy = 0													# Počet umístěných znaků
+	#TODO: použít enum pro hráče, počítač a volné pole 
+	hrac = -1
+	pocitac = 1
 
-	def __init__(self, vyska, sirka):
+	def __init__(self, okno, vyska, sirka):
 		self.platno = Canvas(okno, width = sirka * strana, height = vyska * strana)
 		self.platno.grid()
-		self.platno.bind("<Button-1>", self.hrac)
+		self.platno.bind("<Button-1>", self.tah)
 		self.sirka = sirka
 		self.vyska = vyska
-		for i in range(vyska):
-			# Řádek
+		for x in range(sirka):
+			# Sloupec
 			self.plan.append([])
-			for j in range(sirka):
-				# Sloupec
-				self.plan[i].append(0)
-				self.platno.create_rectangle(j * strana, i * strana,
-				(j + 1) * strana, (i + 1) * strana)
+			for y in range(vyska):
+				# Řádek
+				self.plan[x].append(0)
+				self.platno.create_rectangle(x * strana, y * strana,
+					(x + 1) * strana, (y + 1) * strana)
 
-	def hrac(self, udalost):
+	def tah(self, udalost):
 		# Zisk souřadnic pole
 		pole_x = int(udalost.x / strana)
 		pole_y = int(udalost.y / strana)
-		self.tah(pole_x, pole_y)
+		self.umisteni(pole_x, pole_y)
 		# Další tah
-		nejlepsi = 0
-		x = 0
-		y = 0
-		for i in range(self.vyska):
-			for j in range(self.sirka):
-				if self.plan[i][j] == 0:
-					# Dočasné umístění
-					self.plan[i][j] = 2
-					ohodnoceni = self.minimax(1, self.plan, 2)
-					# Odebrání
-					self.plan[i][j] = 0
-					if ohodnoceni < nejlepsi:
-						nejlepsi = ohodnoceni
-						y = i
-						x = j
-		self.tah(x, y)
+		ohodnoceni = self.minimax(3, self.plan, self.pocitac)
+		self.umisteni(ohodnoceni[0], ohodnoceni[1])
 
-	def tah(self, pole_x, pole_y):
-		if self.plan[pole_y][pole_x] != 0:						# Je dané pole volné?
+	def umisteni(self, pole_x, pole_y):
+		if self.plan[pole_x][pole_y] != 0:						# Je dané pole volné?
 			print("Pole je obsazené!")
 			return
 
+		#TODO: použít proměnnou hráč_na_tahu místo tahy?
 		if self.tahy % 2 == 0:									# Hráč
 			self.kolecko(pole_x, pole_y)
-			self.plan[pole_y][pole_x] = 1
+			self.plan[pole_x][pole_y] = self.hrac
 		else:													# Počítač
 			self.krizek(pole_x, pole_y)
-			self.plan[pole_y][pole_x] = 2
+			self.plan[pole_x][pole_y] = self.pocitac
 		self.tahy += 1
 
+		#TODO: parametrizovat počet potřebný pro vítězství
 		if self.pocet_v_rade(pole_x, pole_y, self.plan) == 3:
-			if self.plan[pole_y][pole_x] == 1:
+			if self.plan[pole_x][pole_y] == self.hrac:
 				print("Vyhrál hráč!")
 			else:
 				print("Vyhrál počítač!")
@@ -78,59 +69,92 @@ class Piskvorky:
 	def kolecko(self, x, y):
 		self.platno.create_oval((x * strana) + 5, (y * strana) + 5, ((x + 1) * strana) - 5, ((y + 1) * strana) - 5, outline = "red", width = 2)
 
-	def minimax(self, hloubka, plan, hrac):		
-		if hrac == 1:
-			hodnota = math.inf									# Hráč
-			for i in range(self.vyska):
-				for j in range(self.sirka):
-					if plan[i][j] == 0:
-						plan[i][j] = hrac
-						if hloubka == 0:
-							hodnota = min(hodnota, -self.pocet_v_rade(i, j, plan))
-						else:
-							hodnota = min(hodnota, self.minimax(hloubka - 1, plan, 2))
-						plan[i][j] = 0
-			print(hodnota)
-			return hodnota
-		else:
-			hodnota = -math.inf									# Počítač
-			for i in range(self.vyska):
-				for j in range(self.sirka):
-					if plan[i][j] == 0:
-						plan[i][j] = hrac
-						if hloubka == 0:
-							hodnota = max(hodnota, self.pocet_v_rade(i, j, plan))
-						else:
-							hodnota = max(hodnota, self.minimax(hloubka - 1, plan, 1))
-						plan[i][j] = 0
-			print(hodnota)
-			return hodnota
+	def minimax(self, hloubka, plan, hrac_na_tahu):		
+		if hrac_na_tahu == self.hrac:							# Hráč
+			nejlepsi = [-1, -1, math.inf]
+		else:													# Počítač
+			nejlepsi = [-1, -1, -math.inf]
+		
+		#TODO: přidat kontrolu i dalších konců hry (nejen remízy)
+		if self.tahy == self.vyska * self.sirka:
+			return [-1, -1, 0]
+
+		for x in range(self.sirka):
+			for y in range(self.vyska):
+				if plan[x][y] == 0:
+					# Dočasné umístění
+					plan[x][y] = hrac_na_tahu
+					self.tahy += 1
+					if hloubka == 0:
+						ohodnoceni = [x, y, self.ohodnoceni_planu(plan)]
+					else:
+						ohodnoceni = [x, y, self.minimax(hloubka - 1, plan, -hrac_na_tahu)[2]]
+					# Odebrání
+					plan[x][y] = 0
+					self.tahy -= 1
+
+					if hrac_na_tahu == self.hrac:
+						if ohodnoceni[2] < nejlepsi[2]:
+							nejlepsi = ohodnoceni
+					else:
+						if ohodnoceni[2] > nejlepsi[2]:
+							nejlepsi = ohodnoceni
+
+				# TODO: parametrizovat počet potřebný pro výhru
+				elif self.pocet_v_rade(x, y, plan) == 3:
+					if plan[x][y] == self.hrac:
+						return [-1, -1, -math.inf]
+					else:
+						return [-1, -1, math.inf]						
+
+		return nejlepsi
 
 	def pocet_v_rade(self, pole_x, pole_y, plan):
-		maximum = - math.inf
+		maximum = -math.inf
 
-		# Kontrolování všech možných směrů {nahoru, doleva, doleva nahoru, doprava nahoru}
+		# Kontrolování všech možných směrů {dolu, doleva, doleva dolu, doprava dolu}
 		for i in [ [0, 1], [-1, 0], [-1, 1], [1, 1] ]:
 			v_rade = 1
+
 			j = 1
 			while self.je_na_planu(pole_x + (i[0] * j), pole_y + (i[1] * j)) and \
-					plan[pole_y][pole_x] == plan[pole_y + (i[1] * j)][pole_x + (i[0] * j)]:
+					plan[pole_x][pole_y] == plan[pole_x + (i[0] * j)][pole_y + (i[1] * j)]:
 				v_rade += 1
 				j += 1
+
 			j = 1
 			while self.je_na_planu(pole_x - (i[0] * j), pole_y - (i[1] * j)) and \
-					plan[pole_y][pole_x] == plan[pole_y - (i[1] * j)][pole_x - (i[0] * j)]:
+					plan[pole_x][pole_y] == plan[pole_x - (i[0] * j)][pole_y - (i[1] * j)]:
 				v_rade += 1
 				j += 1
+
 			if v_rade > maximum:
 				maximum = v_rade
-		print(maximum)
+
 		return maximum
-	
+
+	def ohodnoceni_planu(self, plan):
+		pocitac_max = -math.inf
+		hrac_max = -math.inf
+
+		for x in range(self.sirka):
+			for y in range(self.vyska):
+				if plan[x][y] == self.hrac:
+					hodnota = self.pocet_v_rade(x, y, plan)
+					if hodnota > hrac_max:
+						hrac_max = hodnota
+					
+				elif plan[x][y] == self.pocitac:
+					hodnota = self.pocet_v_rade(x, y, plan)
+					if hodnota > pocitac_max:
+						pocitac_max = hodnota
+
+		return pocitac_max - hrac_max
+
 	def je_na_planu(self, pole_x, pole_y):
-		if pole_x < 0 or pole_x >= len(self.plan[0]):
+		if pole_x < 0 or pole_x >= self.sirka:
 			return False
-		if pole_y < 0 or pole_y >= len(self.plan):
+		if pole_y < 0 or pole_y >= self.vyska:
 			return False
 		else:
 			return True
@@ -139,10 +163,7 @@ class Piskvorky:
 okno = Tk()
 
 
-try:
-	hra = Piskvorky(3,3)
-except ValueError as v:
-	print(v)
-	exit()
+#TODO: pocet kamenu potrebnych pro vitezstvi jako parametr
+hra = Piskvorky(okno, 3, 3)
 
 okno.mainloop()
