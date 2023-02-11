18f3fb3060fdd3abb32f02fa7711fdb7091d1eff
Eliška Králová <kralovaeliska3@natur.cuni.cz>
2023-02-11 10:43:37 +0100

    Doplnění komentářů

--- piskvorky.py
+++ piskvorky.py
@@ -36,7 +36,7 @@ class Piskvorky:
 		
 		self.platno = Canvas(okno, width = sirka * strana, height = vyska * strana)
 		self.platno.grid()
-		self.platno.bind("<Button-1>", self.tah)
+		self.platno.bind("<Button-1>", self.tah)				# Po kliknutí zavolá funkci tah
 		self.sirka = sirka
 		self.vyska = vyska
 		self.pocet_vyhra = pocet_vyhra
@@ -63,6 +63,11 @@ class Piskvorky:
 			self.umisteni(ohodnoceni[0], ohodnoceni[1])
 
 	def umisteni(self, pole_x, pole_y):
+		'''
+		Umístění kamene na hrací plán na dané souřadnice
+		pole_x: x-souřadnice pole
+		pole_y: y-souřadnice pole
+		'''
 		if self.tahy % 2 == 0:									# Hráč
 			self.kolecko(pole_x, pole_y)
 			self.plan[pole_x][pole_y] = Pole.hrac
@@ -79,24 +84,44 @@ class Piskvorky:
 				if messagebox.showinfo(title="Konec hry", message="Prohráli jste!"):
 					exit()
 				
-		elif self.tahy == self.vyska * self.sirka:
+		elif self.tahy == self.vyska * self.sirka:				# Zaplněný plán - remíza
 			if messagebox.showinfo(title="Konec hry", message="Remíza!"):
 				exit()
 
 	def krizek(self, x, y):
+		'''
+		Nakreslení křížku do daného pole
+		x: x-souřadnice pole
+		y: y-souřadnice pole
+		'''
 		self.platno.create_line((x * strana) + 5, (y * strana) + 5, ((x + 1) * strana) - 5, ((y + 1) * strana) - 5, fill="blue", width=2)
 		self.platno.create_line((x * strana) + 5, ((y + 1) * strana) - 5, ((x + 1) * strana) - 5, (y * strana) + 5, fill="blue", width=2)
 
 	def kolecko(self, x, y):
+		'''
+		Nakreslení kolečka do daného pole
+		x: x-souřadnice pole
+		y: y-souřadnice pole
+		'''
 		self.platno.create_oval((x * strana) + 5, (y * strana) + 5, ((x + 1) * strana) - 5, ((y + 1) * strana) - 5, outline = "red", width = 2)
 
-	def minimax(self, hloubka, plan, hrac_na_tahu, alfa, beta):		
+	def minimax(self, hloubka, plan, hrac_na_tahu, alfa, beta):
+		'''
+		Vybere nejvýhodnější pole pro umístění kamene
+		hloubka: hloubka propočtu, obtížnost
+		plan: hrací plán
+		hrac_na_tahu: hráč na tahu, pro kterého byla funkce volána
+		alfa: minimální možné skóre maximalizujícího hráče
+		beta: maximální možné skóre minimalizujícího hráče
+		return: trojice [x-souřadnice, y-souřadnice, ohodnocení plánu] nejvýhodnějšího pole
+		'''
+		# Počáteční hodnoty
 		if hrac_na_tahu == Pole.hrac:							# Hráč
 			nejlepsi = [-1, -1, math.inf]
 		else:													# Počítač
 			nejlepsi = [-1, -1, -math.inf]
 		
-		if self.tahy == self.vyska * self.sirka:
+		if self.tahy == self.vyska * self.sirka:				# Remíza
 			return [-1, -1, 0]
 
 		# Náhodný výběr pořadí polí
@@ -119,7 +144,7 @@ class Piskvorky:
 					self.tahy -= 1
 
 					if hrac_na_tahu == Pole.hrac:
-						# Přepsání starší hodnoty - jinak může zůstat počáteční hodnota [-1, -1, -math.inf]
+						# Přepsání starší hodnoty - jinak může zůstat počáteční hodnota [-1, -1, math.inf]
 						if ohodnoceni[2] <= nejlepsi[2]:
 							nejlepsi = ohodnoceni
 
@@ -127,6 +152,7 @@ class Piskvorky:
 							return nejlepsi
 						beta = min(beta, nejlepsi[2])
 					else:
+						# Přepsání starší hodnoty - jinak může zůstat počáteční hodnota [-1, -1, -math.inf]
 						if ohodnoceni[2] >= nejlepsi[2]:
 							nejlepsi = ohodnoceni
 
@@ -143,6 +169,13 @@ class Piskvorky:
 		return nejlepsi
 
 	def pocet_v_rade(self, pole_x, pole_y, plan):
+		'''
+		Vypočítání počtu kamenů v řadě na hracím plánu pro umisťovaný kámen
+		pole_x: x-souřadnice pole
+		pole_y: y-souřadnice pole
+		plan: hrací plán
+		return: maximální počet kamenů v řadě
+		'''
 		maximum = -math.inf
 
 		# Kontrolování všech možných směrů {dolu, doleva, doleva dolu, doprava dolu}
@@ -167,6 +200,12 @@ class Piskvorky:
 		return maximum
 
 	def ohodnoceni_planu(self, plan):
+		'''
+		Ohodnocení každého pole na hracím plánu dle počtu kamenů v řadě pro jednotlivé hráče
+		plan: hrací plán
+		return: rozdíl počtu kamenů v řadě mezi hráči
+		'''
+		# Nejmenší počáteční hodnota
 		pocitac_max = -math.inf
 		hrac_max = -math.inf
 
@@ -185,6 +224,12 @@ class Piskvorky:
 		return pocitac_max - hrac_max
 
 	def je_na_planu(self, pole_x, pole_y):
+		'''
+		Je pole na hracím plánu?
+		pole_x: x-souřadnice pole
+		pole_y: y-souřadnice pole
+		return: "True" pokud je pole na hracím plánu, jinak "False"
+		'''
 		if pole_x < 0 or pole_x >= self.sirka:
 			return False
 		if pole_y < 0 or pole_y >= self.vyska:
